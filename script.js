0e1323eeceb9ca4614509bcab3b126080d4459ef
Muhammad Dedishov <moxmad99@mail.ru>
2023-01-08 18:25:35 +0300

    Готово

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 const getData = () => {
-    return fetch('db.json')
-        .then(response => response.json())
-        .catch(error => {
-            console.log(error);
-        });
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', 'db.json', true);
+        xhr.onload = () => {
+            if (xhr.status === 200) {
+                resolve(JSON.parse(xhr.responseText));
+            } else {
+                reject(Error(xhr.statusText));
+            }
+        };
+        xhr.onerror = () => reject(Error('Network Error'));
+        xhr.send();
+    });
 }
 
 const sendData = (url, data) => {
-    return fetch(url, {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-    })
-        .catch(error => console.log(error));
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', url, true);
+        xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+        xhr.onload = () => resolve(xhr.responseText);
+        xhr.onerror = () => reject(Error('Network Error'));
+        xhr.send(JSON.stringify(data));
+    });
 }
 
 let user;
@@ -25,5 +33,9 @@ getData()
         console.log(user);
         return sendData('https://jsonplaceholder.typicode.com/posts', user);
     })
-    .then(response => response.json())
-    .then(data => console.log(data));
+    .then(response => {
+        console.log(response);
+    })
+    .catch(error => {
+        console.log(error);
+    });
